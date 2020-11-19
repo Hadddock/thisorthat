1b06dff2f79e324fdfb1825ea28e88b6384e9f77
Elisabeth Jewett <elisabethjewett@gmail.com>
2020-11-19 12:58:35 -0800

    A new getter has been added along with a new global variable to check if a file exists to be able to be loaded, created, or edited.

--- src/thisorthat/Game.java
+++ src/thisorthat/Game.java
@@ -1,10 +1,72 @@
 package thisorthat;
 
 public class Game {
+	
+	public Maze myMaze;
+	public Display myDisplay;
+	
+	boolean isInMaze;
+	boolean isInQuestionMenu;
+	boolean isInPauseMenu;
+	boolean fileExists;
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+	}
+	
+	public Game(Maze theMaze, Display theDisplay) {
+		
+	}
+	
+	public int promptMovement() {
+		return 0;
+	}
+	
+	public boolean promptQuestion(int theRoomY, int theRoomX) {
+		return isInQuestionMenu;
+	}
+	
+	public int promptPauseMenu() {
+		return 0;
+	}
+	
+	public void receiveMovementSelection(int move) {
+		if (isInMaze) {
+			move;
+		}
+	}
+	
+	public void receiveAnswerSelection(int answer) {
+		
+	}
+	
+	public void receivePauseSelection(int pause) {
+		isInPauseMenu = true;
+	}
+	
+	public boolean checkWinCondition() {
+		return false;
+	}
+	
+	public boolean checkWinPossible() {
+		return false;
+	}
+	
+	public void saveGame() {
+		fileExists = true;
+	}
+	
+	public void loadGame() {
+		
+	}
+	
+	public boolean checkFileExists() {
+		return fileExists;
+	}
+	
+	public void exitGame() {
+		
 	}
 
 }
